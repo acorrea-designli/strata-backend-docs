1471d3438ea29c264ffb2c95090afed26e840cd4
z4yross <z4yross@gmail.com>
2024-03-11 18:21:03 -0500

    actions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,15 +9,16 @@ const config: Config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://your-docusaurus-site.example.com",
+	url: "https://acorrea-designli.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/strata-backend-docs/",
+  trailingSlash: false,
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "designli", // Usually your GitHub org/user name.
-	projectName: "strata-backend", // Usually your repo name.
+	organizationName: "acorrea-designli", // Usually your GitHub org/user name.
+	projectName: "strata-backend-docs", // Usually your repo name.
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
